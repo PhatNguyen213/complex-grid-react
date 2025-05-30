98f7c172e8aae9ac13934394607519919ee47dce
Phat Nguyen <preddevil@gmail.com>
2025-05-30 09:59:31 +0700

    update jest config coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 const config = {
   testEnvironment: "jest-environment-jsdom",
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      lines: 70,
+    },
+  },
 };
 
 export default config;
